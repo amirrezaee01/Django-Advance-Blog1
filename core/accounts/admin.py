b966ad4d476fcefac72d0d83da6573e96278b1d5
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-05-23 11:37:36 +0330

    fix the admin panel for accounts user

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_active', 'is_superuser', 'is_staff')
+    list_filter = ('email', 'is_superuser', 'is_staff')
+    search_fields = ('email',)
+    ordering = ('email',)
+    # to show the informathin of the user in admin panel
+    fieldsets = (
+        ('Authentication', {'fields': ('email', 'password')}),
+        # Add permissions if you want to manage them
+        ('Permissions', {'fields': ('is_active', 'is_staff',
+                                    'is_superuser', 'user_permissions')}),
+        ('important date', {'fields': ('last_login',)}),
+
+    )
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2", 'is_active', 'is_staff',
+                                    'is_superuser']
+            },
+        ),
+    ]
+
+
+admin.site.register(User, CustomUserAdmin)
