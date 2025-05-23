b30c7904221716c7a6785ba6dc223371e2e27047
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-05-23 14:10:19 +0330

    migrate the model of blog

--- core/core/settings.py
+++ core/core/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "accounts",
-    # "blog",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -117,10 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
