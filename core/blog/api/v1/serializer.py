d39e07f7823a92c861681e11d589f4a58b2a619a
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-01 16:42:43 +0330

    Refactor PostSerializer and update blog API views
    Replaced the custom PostSerializer with a ModelSerializer to leverage Django model fields. Updated post_list and PostDetail views to support POST and PUT methods, enabling creation and update of posts via the API.

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from ...models import Post
+
+# class PostSerializer(serializers.Serializer):
+#     author = serializers.CharField(max_length=100)
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
 
-class PostSerializer(serializers.Serializer):
-    author = serializers.CharField(max_length=100)
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", 'author', "title", "content", "status",
+                  "created_date", "published_date",]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,15 +5,27 @@ from ...models import Post
 from django.shortcuts import get_object_or_404
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def post_list(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def PostDetail(request, id):
     post = get_object_or_404(Post, pk=id, status=True)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
