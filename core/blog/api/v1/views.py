14d327958bb1d92c562d528f33164050414c0adc
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-08 17:34:37 +0330

    Add filtering, search, and ordering to Post API
    Integrated DjangoFilterBackend, SearchFilter, and OrderingFilter into the PostModelViewSet to allow filtering by category, author, and status, searching by title and content, and ordering by published_date. Also added 'django_filters' to INSTALLED_APPS in settings.

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,12 +9,18 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import viewsets
 from .permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class PostModelViewSet(viewsets.ModelViewSet, IsOwnerOrReadOnly):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category', 'author', 'status']
+    search_fields = ['title', 'content']
+    OrderingFilter = ['published_date']
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
