5f9cc7fb5a3defbda63786ab1675db126517e2c0
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-07-08 16:25:45 +0330

    Enhance Post serializer with URLs and snippet fields
    Added snippet, relative_url, and absolute_url fields to the PostSerializer. Implemented custom to_representation logic to tailor output for detail and list views. Introduced get_snippet and get_absolute_url methods to the Post model for improved API responses. Category is now serialized within PostSerializer.

--- core/blog/api/v1/serializer.py
+++ core/blog/api/v1/serializer.py
@@ -1,20 +1,40 @@
 from rest_framework import serializers
 from ...models import Post, Category
 
-# class PostSerializer(serializers.Serializer):
-#     author = serializers.CharField(max_length=100)
-#     id = serializers.IntegerField()
-#     title = serializers.CharField(max_length=255)
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ["id", 'author', "title", "content", "status",
-                  "created_date", "published_date",]
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(
+        source='get_absolute_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField(
+        method_name='get_abs_url')
+
+    class Meta:
+        model = Post
+        fields = ["id", 'author', "title", "content", "status", 'category', "snippet", "relative_url",
+                  "absolute_url", "created_date", "published_date",]
+
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
+    # Override the to_representation method to customize the output
+    # of the serializer if needed.seperate the two part
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+        else:
+            rep.pop('content', None)
+
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # getting user model
 
 
@@ -17,6 +18,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_snippet(self):
+        return self.content[:10] + '...'
+
+    def get_absolute_url(self):
+        return reverse("blog:api-v1:post-detail", kwargs={"pk": self.pk})
 
 
 class Category(models.Model):
